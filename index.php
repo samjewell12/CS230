1c5474c1a132b0bc550846142163661b42623c3f
Samuel Jewell <sjj0007@mix.wvu.edu>
2021-02-05 21:44:32 -0500

    Lab 2

---
+++ index.php
@@ -0,0 +1,70 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+
+    <div class="about">
+        <hl>
+            About Us
+        </hl>
+        <p>asasdkjfhsdjhgadskjhgkajsdhjkgahjkdshgdkjagsdhjgdsjkhdkjhsadkjfhasdkjf</p>
+    
+    </div>
+
+    <div class="row">
+        <div class= "column">
+            <div class= "card">
+                <img src="images/1.png">
+                <div class= "container">
+                    <h2>CEO</h2>
+                    <p class="c1">Harrison Ford</p>
+                    <p>Email: poopystink@bruh.com</p>
+                </div>
+
+            </div>
+        </div>
+        <div class= "column">
+            <div class= "card">
+                <img src="images/2.jpeg">
+                <div class= "container">
+                    <h2>CEO</h2>
+                    <p>Harrison Ford</p>
+                    <p>Email: poopystink@bruh.com</p>
+                </div>
+
+            </div>
+        </div>
+        <div class= "column">
+            <div class= "card">
+                <img src="images/3.jpg">
+                <div class= "container">
+                    <h2>CEO</h2>
+                    <p>Harrison Ford</p>
+                    <p>Email: poopystink@bruh.com</p>
+                </div>
+
+            </div>
+        </div>
+        <div class= "column">
+            <div class= "card">
+                <img src="images/4.jpg">
+                <div class= "container">
+                    <h2>CEO</h2>
+                    <p>Harrison Ford</p>
+                    <p>Email: poopystink@bruh.com</p>
+                </div>
+
+            </div>
+        </div>
+        <div class= "column">
+            <div class= "card">
+                <img src="images/5.jpeg">
+                <div class= "container">
+                    <h2>CEO</h2>
+                    <p>Harrison Ford</p>
+                    <p>Email: poopystink@bruh.com</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
